83ddfda0468cc02c10ab6aeaad8e6542a77cbdc5
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-08-25 15:47:53 -0400

    consolidation of files

---
+++ fizzbuzz.js
@@ -0,0 +1,30 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+    
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>Document</title>
+// </head>
+
+// <script>
+
+
+for (var i = 0; i <+100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log (“FIZZBUZZ!“)
+    } else if (i % 3 === 0) {
+        console.log (“fizz”)
+    } else if (i % 5 === 0) {
+        console.log (“buzz”)
+    } else {
+        console.log (i)
+    }
+ }
+    }
+    }
+
+    }
+fizzBuzz(100)
+  
+// </script>
